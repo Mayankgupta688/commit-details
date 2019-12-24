57926615e2cde423781ca0eae3ee137583f2a3d2
Mayank <mayank.gupta@iongroup.com>
2019-12-24 10:21:46 +0530

    Small Updates

--- src/app/components/headerComponent/header.component.ts
+++ src/app/components/headerComponent/header.component.ts
@@ -8,4 +8,9 @@ import RepoDetails from "../../entities/RepoDetails";
 })
 export default class HeaderComponent {
     @Input() repositorydetails: RepoDetails;
+
+    capitalize(stringInput: string) {
+      if (typeof stringInput !== 'string') return ''
+      return stringInput.charAt(0).toUpperCase() + stringInput.slice(1)
+  }
 }
